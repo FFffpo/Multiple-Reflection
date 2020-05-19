5314f2b12fc88e9e7de39276a392fa9664d990e3
FFff <13598539908@163.com>
2020-05-19 16:07:42 +0800

    modified:   TRY.m

--- TRY.m
+++ TRY.m
@@ -4,7 +4,7 @@ abs_Line=sqrt(Line*Line');
 abs_Light=sqrt(Light*Light');
 
 E_Line=Line./abs_Line;     %线的单位方向向量
-V_Line=E_Line*[0 -1;1 0];  %将E_Line逆时针旋转pi/2
+V_Line=E_Line*[0 -1;1 0];  %将E_Line顺时针旋转pi/2
 
 abs_L_Line=(Line*Light')/abs_Line;
 abs_V_Line=sqrt(abs_Light*abs_Light-abs_L_Line*abs_L_Line);
